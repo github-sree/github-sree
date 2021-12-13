e35d2d9d8ef7c45d6138f6975e8dbd58893c80c9
sreedhar <69903740+github-sree@users.noreply.github.com>
2021-12-13 16:32:20 +0530

    Update SpringAppApplication.java

--- src/main/java/com/example/demo/SpringAppApplication.java
+++ src/main/java/com/example/demo/SpringAppApplication.java
@@ -2,12 +2,26 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String HellotoOpenShift() {
+		return "Welcome to Openshift Deployment";
+	}
+
+	@GetMapping("/{name}")
+	public String HellotoOpenShift(@PathVariable String name) {
+		return "Hi " + name + ", Welcome to Openshift Deployment";
 	}
 
 }
