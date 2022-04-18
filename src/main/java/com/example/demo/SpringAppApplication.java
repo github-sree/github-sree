dab05664f5c745776b7ae456ab63384e18639b79
Varun Suresh <varun.suresh@dhl.com>
2022-04-18 16:21:43 +0530

    deployment conf and logback added

--- src/main/java/com/example/demo/SpringAppApplication.java
+++ src/main/java/com/example/demo/SpringAppApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +13,21 @@ import java.util.Date;
 @RestController
 public class SpringAppApplication {
 
+	Logger logger = LoggerFactory.getLogger(SpringAppApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
 	}
 
 	@GetMapping("/")
 	public String HellotoOpenShift() {
-		return "Welcome to Openshift Deployment-Edited"+new Date();
+		logger.info("triggered {}", new Date());
+		return "Welcome to Openshift Deployment-Edited" + new Date();
 	}
 
 	@GetMapping("/{name}")
 	public String HellotoOpenShift(@PathVariable String name) {
+		logger.info("triggered {},{}", name, new Date());
 		return "Hi " + name + ", Welcome to Openshift Deployment";
 	}
 
