3786efc3bb6a09a3199db8a7ba05ac45f7538a91
sree <sree@sree-corei3>
2022-04-22 23:40:42 +0530

    code changes

--- src/main/java/com/example/demo/SpringAppApplication.java
+++ src/main/java/com/example/demo/SpringAppApplication.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @RestController
 public class SpringAppApplication {
 
